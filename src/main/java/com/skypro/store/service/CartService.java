a8a275d4f47b8dd489cf5f0a6059c9cdf4c6eebd
Merdan Charliev <mcharliev@gmail.com>
2022-11-29 18:44:28 +0300

    Store-oder-H_W

--- src/main/java/com/skypro/store/service/CartService.java
+++ src/main/java/com/skypro/store/service/CartService.java
@@ -8,5 +8,17 @@ import java.util.List;
 @Service
 public class CartService {
 
+    private final CartRepository cartRepository;
 
+    public CartService(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+    public void addToCart(List<Integer> ids) {
+        this.cartRepository.addToCart(ids);
+    }
+
+    public List<Integer> getCartContent() {
+        return this.cartRepository.getCartContent();
+    }
 }
